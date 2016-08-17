3e3ad2a417e3b9b5f2d657b36e711dc0a74cacf1
peter <pesegal@gmail.com>
2016-08-16 19:20:51 -0700

    Adjusting for other types

--- drawing.py
+++ drawing.py
@@ -5,12 +5,14 @@ from kivy.properties import ObjectProperty
 from kivy.config import Config
 
 
+
 class DrawingSpace(Button):
     current_color = ObjectProperty((1, 1, 1, 1))
 
     def __init__(self):
         super(DrawingSpace, self).__init__()
         self.selected_color_index = 0
+        self.multi_line_height = 0  # This is to smooth out the toggle between shorten.
         self.colors = [
             (1, 0, 0, 1),
             (0, 1, 0, 1),
@@ -22,6 +24,8 @@ class DrawingSpace(Button):
             (0, 0, 0, 1)
         ]
 
+        # Return a list of only inheirted label attributes
+
         self.tasktext = Label()
         self.add_widget(self.tasktext)
         self.tasktext.text = "This is a really long string for testing. We want the text to correctly align."
@@ -31,28 +35,30 @@ class DrawingSpace(Button):
         self.bind(size=self.size_change)
 
     def cycle_colors(self):
-        if self.selected_color_index >= len(self.colors) - 1:
-            print(len(self.colors))
-            self.selected_color_index = 0
-        self.selected_color_index += 1
-        print(self.selected_color_index)
-        self.current_color = self.colors[self.selected_color_index]
-        self.tasktext.shorten = not self.tasktext.shorten
+        pass
+        # if self.selected_color_index >= len(self.colors) - 1:
+        #     print(len(self.colors))
+        #     self.selected_color_index = 0
+        # self.selected_color_index += 1
+        # print(self.selected_color_index)
+        # self.current_color = self.colors[self.selected_color_index]
+        # self.tasktext.shorten = not self.tasktext.shorten
 
-    def size_change(self, _object, size):
+    def size_change(self, _object, size, short_padding=20):
         start = self.width*.05 + 5
 
         self.tasktext.width = self.width - start - 20
         self.tasktext.height = self.height
         self.tasktext.text_size = (self.width - start - 40, None)
 
-        print("Text size:    {}".format(self.tasktext.text_size))
-        print("Texture size: {}".format(self.tasktext.texture_size))
-        if self.tasktext.texture_size[1] + 40 >= self.height and self.shorten == False:
-            print("True")
+        if not self.tasktext.shorten:
+            self.multi_line_height = self.tasktext.texture_size[1] + short_padding
+
+        if self.tasktext.texture_size[1] + short_padding >= self.height and not self.shorten:
             self.tasktext.shorten = True
-        elif self.tasktext.shorten:
+        elif self.multi_line_height < self.height:
             self.tasktext.shorten = False
+
         self.tasktext.x = self.x + start + 20
         self.tasktext.y = self.y
 
